86366265eac2c451232d8fc59d0670dedae94078
MaggieDeSantis <sheisntreal@gmail.com>
2023-03-19 15:49:48 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // code your solution here
+
+function saturdayFun( task = "roller-skate"){
+    return `This Saturday, I want to ${task}!`;
+}
+
+function mondayWork ( task = "go to the office"){
+return `This Monday, I will ${task}.`;
+}
+function wrapAdjective (highlight = "*") {
+const result = function(parameter = "special") {
+return `You are ${highlight}${parameter}${highlight}!`}
+return result;
+}
